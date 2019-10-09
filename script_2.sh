6b0df0a2bb810d0580f7ec6a5dfaf19cb64bde29
Andrew Cameron <acameron949@gmail.com>
2019-10-09 10:02:39 -0400

    added script_2.sh

---
+++ script_2.sh
@@ -0,0 +1,6 @@
+echo "Highest earner"
+cat wages.csv | sort -t , -k 4 -g | tail -n 1
+echo "Lowest earner"
+cat wages.csv | sort -t , -k 4 -g | head -n 2 | tail -n 1
+echo "Females in top 10 earners"
+cat wages.csv | sort -t , -k 4 -g | tail -n 10 | grep "female" | wc -l
